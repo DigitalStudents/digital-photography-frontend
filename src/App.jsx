d48854478b00f89f592c9c9b81e7f49c38ecb7ed
tomyesp <tomyesposito15@gmail.com>
2023-10-23 21:53:13 -0300

    commit para hacer un pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 
 import Home from './Pages/Home/Home'
 import './App.css'
-import ProductDetail from './Pages/ProductDetail/ProductDetail'
-import ProductBox from "./components/ProductBox/ProductBox";
+
 
 function App() {
   return (
