84ab2df1229c0b615b183ee1d40b6f1083086e59
LilianaDurango <93357747+LilianaDurango@users.noreply.github.com>
2023-11-01 21:18:15 -0500

    remove list products from main page

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,31 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import ProductBox from "../ProductBox/ProductBox"
-import './ProductList.css'
+import React from 'react'
 
 const ProductList = () => {
-
-    const [products, setProducts] = useState([])
-
-    const fetchData = async () => {
-        await fetch('http://localhost:8080/v1/productos')
-        .then(response => response.json())
-        .then(data => data)
-    }
-
-    useEffect(() => {
-        fetch('http://localhost:8080/v1/productos')
-            .then(response => response.json())
-            .then(data => setProducts(data))
-    }, [])
-
-    products.forEach(p => console.log(p.imagenes[0]))
-
     return(
-        <div className="product-list">
-            {
-                products.map(p => <ProductBox key={p.id} nombre={p.nombre} id={p.id} description={p.description} imagen={p.imagenes[0]} />)
-            }
-        </div>
+        <div></div>
     )
 }
 
