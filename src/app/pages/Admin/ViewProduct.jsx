e8ebc4ae951b3310df33a37f82a5053d199fe572
LilianaDurango <93357747+LilianaDurango@users.noreply.github.com>
2023-11-06 18:39:23 -0500

    Update ViewProduct.jsx

--- src/app/pages/Admin/ViewProduct.jsx
+++ src/app/pages/Admin/ViewProduct.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import ProductBox from "../../../components/ProductBox/ProductBox"
 import '../../../components/ProductList/ProductList.css'
+import Pagination from './Pagination'
 
 const ViewProduct = () => {
-  const [products, setProducts] = useState([])
+    
+    const [products, setProducts] = useState([])
 
-    const fetchData = async () => {
-        await fetch('http://localhost:8080/v1/productos')
-        .then(response => response.json())
-        .then(data => data)
-    }
+    const [currentPage, setCurrentPage] = useState(1)
+    const [recordsPerPage] = useState(10)
 
     useEffect(() => {
         fetch('http://localhost:8080/v1/productos')
@@ -17,13 +16,26 @@ const ViewProduct = () => {
             .then(data => setProducts(data))
     }, [])
 
-    products.forEach(p => console.log(p.imagenes[0]))
+    const indexOfLastRecord = currentPage * recordsPerPage
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage
+    const currentRecords = products.slice(indexOfFirstRecord, indexOfLastRecord)
+    const nPages = Math.ceil(products.length / recordsPerPage)
 
     return(
-        <div className="product-list">
-            {
-                products.map(p => <ProductBox key={p.id} nombre={p.nombre} id={p.id} description={p.description} imagen={p.imagenes[0]} />)
-            }
+        <div>
+            <div className='container mt-5'>
+            
+                <div className="product-list">
+                    {
+                        currentRecords.map(p => <ProductBox key={p.id} nombre={p.nombre} id={p.id} description={p.description} imagen={p.imagenes[0]} />)
+                    }
+                </div>
+                <Pagination
+                    nPages={nPages}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                />
+            </div>
         </div>
     )
 }
