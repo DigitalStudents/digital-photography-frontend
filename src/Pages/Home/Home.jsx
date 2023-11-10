418f89f902e3dbfe51c200b854403c3ecab38603
tomyesp <tomyesposito15@gmail.com>
2023-11-09 22:24:27 -0300

    diseño de home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from "../../components/SearchSection/SearchBar";
 import ProductList from "../../components/ProductList/ProductList";
+import Hero from "../../components/HeroBanner/Hero";
 
 function Home() {
   return (
     <>
       
-      <SearchBar />
+      <SearchBar /> 
+      <Hero />
       <ProductList />
     </>
   );
