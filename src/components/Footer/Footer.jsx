f8d651abd589c069245f4423169eef5b0b5b1c1e
mariiasol <fmariasol93@gmail.com>
2023-11-15 15:41:49 -0600

    modificacion footer- responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,49 +2,47 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => {
-    return (
-        <footer>
-        <section className="imgfooter">
+  return (
+    <footer>
+      <section className="content">
+        <div className="imgfooter">
           <div className="logofooter">
-            <img src="/src/components/Footer/svg/logo.jpeg"/>
+            <img src="/src/components/Footer/svg/logo.jpeg" alt="Logo" />
           </div>
-          <h1 className="contacto">contacto@filmbook.com </h1>
+          <h1 className="contacto">contacto@filmbook.com</h1>
 
-            <div className="iconosfooter">
-              <a href="https://www.facebook.com"><img src="/src/components/Footer/svg/icon-facebook.svg"/></a>
-              
-              <a href="https://www.twitter.com"><img src="/src/components/Footer/svg/icon-twitter.svg"/></a>
-              
-              <a href="https://www.instagram.com"><img src="/src/components/Footer/svg/icon-instagram.svg"/></a>
-           </div>  
-            
-          </section>
+          <div className="iconosfooter">
+            <a href="https://www.facebook.com"><img src="/src/components/Footer/svg/icon-facebook.svg" alt="Facebook" /></a>
+            <a href="https://www.twitter.com"><img src="/src/components/Footer/svg/icon-twitter.svg" alt="Twitter" /></a>
+            <a href="https://www.instagram.com"><img src="/src/components/Footer/svg/icon-instagram.svg" alt="Instagram" /></a>
+          </div>
+        </div>
 
-          <section className="menufooter">
-            <nav className="navfooter">
-                <ul className="ul2">
-                    <li className="lifooter"><a href="#" className="linkfooter">Soporte</a></li>
-                    <li className="lifooter"><a href="#" className="linkfooter" >Preguntas frecuentes</a></li>
-                    <li className="lifooter"><a href="#" className="linkfooter">Tutoriales</a></li>
-                    <li className="lifooter"><a href="#" className="linkfooter">Mapa del sitio</a></li>
-                </ul>
-            </nav>
+        <div className="menufooter">
+          <nav className="navfooter">
+            <ul className="ul2">
+              <li className="lifooter"><a href="#" className="linkfooter">Soporte</a></li>
+              <li className="lifooter"><a href="#" className="linkfooter">Preguntas frecuentes</a></li>
+              <li className="lifooter"><a href="#" className="linkfooter">Tutoriales</a></li>
+              <li className="lifooter"><a href="#" className="linkfooter">Mapa del sitio</a></li>
+            </ul>
+          </nav>
 
-            <nav className="navfooter">
-                <ul className="ul2">
-                    <li className="lifooter"><a href="#" className="linkfooter">Política de privacidad</a></li>
-                    <li className="lifooter"><a href="#" className="linkfooter">Procesamiento de datos</a></li>
-                    <li className="lifooter"><a href="#" className="linkfooter">Términos y condiciones</a></li>
-                </ul>
-            </nav>
-          <p>
+          <nav className="navfooter">
+            <ul className="ul2">
+              <li className="lifooter"><a href="#" className="linkfooter">Política de privacidad</a></li>
+              <li className="lifooter"><a href="#" className="linkfooter">Procesamiento de datos</a></li>
+              <li className="lifooter"><a href="#" className="linkfooter">Términos y condiciones</a></li>
+            </ul>
+          </nav>
+        </div>
+      </section>
 
-          </p>
-            <p id="copyright"> Copyright 2023. All Rights Reserved 2023
-            </p>
-    </section>
-      </footer>
-    );
-  }
+      <section className="firma">
+        <p id="copyright"> Copyright 2023. All Rights Reserved 2023 </p>
+      </section>
+    </footer>
+  );
+}
 
-export default Footer
+export default Footer;
