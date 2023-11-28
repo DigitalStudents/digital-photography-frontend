ecddcc2218663e4692ef21dbea32b0ba6be8ad81
tomyesp <tomyesposito15@gmail.com>
2023-11-27 21:50:37 -0300

    datepicker para seleccionar fechas

--- src/app/components/DatePickerBookings/DatePicker.jsx
+++ src/app/components/DatePickerBookings/DatePicker.jsx
@@ -1,33 +1,61 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import 'react-datepicker/dist/react-datepicker.css';
+import Swal from "sweetalert2";
+import "react-datepicker/dist/react-datepicker.css";
 
-
-
-const DatePickerForm = ({productId}) => {
+const DatePickerForm = ({ productId }) => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
 
-  const handleStartDateChange = date => {
+  const reservationEndpoint = `${import.meta.env.VITE_BACKEND_URL}reservations`;
+
+  const handleStartDateChange = (date) => {
     setStartDate(date);
   };
 
-  const handleEndDateChange = date => {
+  const handleEndDateChange = (date) => {
     setEndDate(date);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
+    const reservationForm = {
+      productId: productId,
+      startDate: startDate,
+      endDate: endDate,
+    };
     // Aquí podrías enviar las fechas a través de una llamada a API o realizar la acción deseada
     if (startDate && endDate) {
-      console.log('StartDate:', startDate);
-      console.log('EndDate:', endDate);
+      console.log("StartDate:", startDate);
+      console.log("EndDate:", endDate);
       // Aquí puedes realizar la lógica para enviar las fechas a través de una llamada a API o realizar alguna acción
+      fetch(reservationEndpoint, {
+        method: "POST",
+        body: JSON.stringify(reservationForm),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => {
+          if (resp.status === 201) {
+            console.log("Reserva creada");
+            Swal.fire("Reserva creada con éxito!", "", "success");
+            setStartDate(null);
+            setEndDate(null);
+          } else {
+            return resp.json().then((data) => {
+              throw new Error(data.message || "Error en la solicitud");
+            });
+          }
+        })
+        .catch((err) => {
+            console.error(err);
+            Swal.fire("Error al crear reserva", err.message || "Error desconocido", "error");
+        });
     } else {
-      console.log('Por favor selecciona ambas fechas');
+      alert("Por favor selecciona ambas fechas");
     }
   };
-
 
   return (
     <form onSubmit={handleSubmit}>
@@ -36,7 +64,7 @@ const DatePickerForm = ({productId}) => {
         <DatePicker
           selected={startDate}
           onChange={handleStartDateChange}
-          dateFormat="dd-MM-yyyy"
+          dateFormat="yyyy-MM-dd"
         />
       </div>
       <div>
@@ -44,7 +72,7 @@ const DatePickerForm = ({productId}) => {
         <DatePicker
           selected={endDate}
           onChange={handleEndDateChange}
-          dateFormat="dd-MM-yyyy"
+          dateFormat="yyyy-MM-dd"
         />
       </div>
       <button type="submit">Enviar</button>
