91262fcf4e264105000ef1d528a347d9d98ead23
StonyKing <ramiro_hero@hotmail.com>
2023-12-10 20:36:21 -0300

    Going back

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png'],
   preview: {
     host: true,
     port: 5173, // This is the port which we will use in docker
